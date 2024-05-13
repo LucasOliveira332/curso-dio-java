00c5b98507388cf75a53eefbe46ba4d3a93cd219
Lucas Gomes <lucas.g.oliveira1@hotmail.com>
2024-05-13 07:19:52 -0300

    add: aula anatomia das classes 3

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         final String BR = "Brasil";
 
-        System.out.println("Hello World! " + BR);
+        System.out.println("Hello World! ".concat(BR));
     }
+
 }
